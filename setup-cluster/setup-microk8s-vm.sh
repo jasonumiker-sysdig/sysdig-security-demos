e75845712c03ed39f22cb55b969ef2ad67695fec
Jason Umiker <jason.umiker@sysdig.com>
2022-11-28 21:52:37 +1100

    Transferring sysdig-agent-helm-install.sh

--- setup-cluster/setup-microk8s-vm.sh
+++ setup-cluster/setup-microk8s-vm.sh
@@ -5,11 +5,13 @@ multipass delete microk8s-vm
 multipass purge
 
 # Provision your local cluster VM
-multipass launch --cpus 2 --mem 4G --disk 20G --name microk8s-vm 22.04
+multipass launch --cpus 4 --mem 8G --disk 20G --name microk8s-vm 22.04
 
 # Deploy and run setup-microk8s.sh to our new VM
 multipass transfer ./bootstrap-microk8s-vm.sh microk8s-vm:/home/ubuntu/
+multipass transfer ./sysdig-agent-helm-install.sh microk8s-vm:/home/ubuntu/
 multipass exec microk8s-vm -- chmod +x /home/ubuntu/bootstrap-microk8s-vm.sh
+multipass exec microk8s-vm -- chmod +x /home/ubuntu/sysdig-agent-helm-install.sh
 multipass exec microk8s-vm -- /home/ubuntu/bootstrap-microk8s-vm.sh
 
 # Copy the .kube/config to the local machine
